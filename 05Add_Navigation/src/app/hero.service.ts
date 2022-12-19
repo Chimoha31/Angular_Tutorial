a081b5913cb47a0cae08efe1a86e8f269ea09a93
Chimoha31 <mokochii1108@gmail.com>
2022-12-18 20:54:15 -0800

    Created dashboard component

--- 05Add_Navigation/src/app/hero.service.ts
+++ 05Add_Navigation/src/app/hero.service.ts
@@ -9,8 +9,8 @@ import { MessageService } from './message.service';
 @Injectable({
   providedIn: 'root',
 })
-export class HeroService {
 
+export class HeroService {
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
